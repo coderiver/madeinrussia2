cb191ad425ab0ff089fcf6e0899aa8598e455507
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-03 14:51:28 +0300

    about.html

--- js/common.js
+++ js/common.js
@@ -38,6 +38,29 @@ $(document).ready(function() {
         event.stopPropagation();
     });
 
+
+    // tabs
+
+    function tab() {
+        $(".js-tab").each(function(){
+            var tab_link = $(this).find("a");
+            var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+            tab_cont.hide();
+            $(this).parents(".js-tab-group").find(".js-tab1").show();
+                tab_link.bind("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parents(".js-tab-group").find("."+index).show();
+                return false;
+            });
+        });
+    } 
+    tab();
+    
     $(document).click(function(event){
       $(".js-select-list").slideUp('fast');
 	});
