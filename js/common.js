a9fb6bb8fa1410aaef73a20096f96cf6b71dd5cc
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-07 12:12:08 +0300

    lang select

--- js/common.js
+++ js/common.js
@@ -84,12 +84,47 @@ $(document).ready(function() {
     } 
     tab();
 
-    // lang
+    // select langueage
 
-    $(".js-lang").on('click', function(event){
-        $(this).toggleClass('is-open');
-        event.stopPropagation(); 
+    function select_lang() {
+        $(".js-lang").each(function(){
+            var select_list = $(this).find(".js-lang-list");
+            var text = select_list.find("li").first().text();
+            select_list.hide();
+            //$(this).find(".js-select-text").text(text);
+            $(this).click(function(event){
+                if ($(this).hasClass("is-active")) {
+                    $(this).removeClass("is-active");
+                    select_list.slideUp("fast");
+                }
+                else {
+                    $(".js-lang").removeClass("is-active");
+                    $(".js-lang-list").hide();
+                    select_list.slideDown("fast");
+                    $(this).addClass("is-active");
+                }
+                event.stopPropagation();
+            });
+            select_list.find("li").click(function(event) {
+                var id = $(this).attr("data-id");
+                var text = $(this).text();
+                var text_short = text.substr(0,3);
+                $(this).parent().parent().find(".js-lang-text").text(text_short);
+                $(this).parent().parent().find(".js-lang-input").val(id);
+                $(this).parent().hide();
+                $(this).parents(".js-lang").removeClass("is-active");
+                event.stopPropagation();
+                return false;
+            });
+        });
+
+    }
+    select_lang();
+    $('.js-lang').click(function(event){
+        event.stopPropagation();
     });
+    
+    
     
     // body scroll-top  
     
